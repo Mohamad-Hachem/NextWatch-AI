764d42d5016e86c9fe086b57a00ae016a5319cb5
Mohamad-Hachem <mohamad27hachem@outlook.com>
2026-06-11 15:22:00 +0200

    adding CRUD operations

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from nextwatch-ai!")
-
-
-if __name__ == "__main__":
-    main()
--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 # creating our Fastapi application 
@@ -40,3 +40,34 @@ async def get_movie(movie_name: str):
 async def testing(random_name):
     """this is for testing"""
     return {'message': f'testing {random_name}'}
+
+
+@app.get("/movies/2")
+async def read_author_category_by_query(movies_name: str, category: str):
+    movies_to_return = []
+    for book in MOVIES:
+        if book.get('title').casefold() == movies_name.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            movies_to_return.append(book)
+
+    return movies_to_return
+
+
+@app.post("/movies/create_movie")
+async def create_movie(new_movie=Body()):
+    MOVIES.append(new_movie)
+
+
+@app.put("/movies/update_movie")
+async def update_book(updated_book=Body()):
+    for i in range(len(MOVIES)):
+        if MOVIES[i].get('title').casefold() == updated_book.get('title').casefold():
+            MOVIES[i] = updated_book
+
+
+@app.delete("/movies/delete_movie/{movie_name}")
+async def delete_book(movie_name: str):
+    for i in range(len(MOVIES)):
+        if MOVIES[i].get('title').casefold() == movie_name.casefold():
+            MOVIES.pop(i)
+            break
