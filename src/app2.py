840906329ceac73a9ce0f70b6e9447ef1c4023e7
Mohamad-Hachem <mohamad27hachem@outlook.com>
2026-06-12 12:37:30 +0200

    adding HTTPExceptions and the right status_code for our apis

--- src/app2.py
+++ src/app2.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from movie import Movie, MovieRequest
 from helper import give_next_proper_id
+from starlette import status
 
 
 app = FastAPI()
@@ -14,22 +15,22 @@ MOVIES = [
     Movie(6,"title 6", "auth 1", "love", "this is a nice book", 4),
 ]
 
-@app.get("/movies")
+@app.get("/movies", status_code=status.HTTP_200_OK)
 def get_all_movies():
     """this api endpoint to return all movies"""
     return MOVIES
 
 
-@app.get("/movies/{movie_id}")
+@app.get("/movies/{movie_id}", status_code=status.HTTP_200_OK)
 def get_movie(movie_id: int= Path(gt=0)):
     """this api endpoint to return a specific movie"""
     for movie in MOVIES:
         if movie.id == movie_id:
             return movie
-    return "No movies found"
+    raise HTTPException(status_code=404, detail="item not found")
 
 
-@app.get("/movies/")
+@app.get("/movies/", status_code=status.HTTP_200_OK)
 def get_movies_with_specific_rating(movie_rating: int= Query(gt=-1,lt=6)):
     """this api endpoint to fet movies with specific reading"""
     final_movies_list = []
@@ -39,24 +40,32 @@ def get_movies_with_specific_rating(movie_rating: int= Query(gt=-1,lt=6)):
     return final_movies_list
 
 
-@app.put("/movies/update_movie")
+@app.put("/movies/update_movie", status_code=status.HTTP_204_NO_CONTENT)
 def update_movie(movie: MovieRequest):
     """updating a book with a specific ID"""
+    movie_updated = False
     for i in range(len(MOVIES)):
         if MOVIES[i].id == movie.id:
             MOVIES[i] = Movie(**movie.model_dump())
+            movie_updated = True
+    if not movie_updated:
+        raise HTTPException(status_code=404, detail="item not found")
 
 
-@app.delete("/movies/{movie_id}")
+@app.delete("/movies/{movie_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_movie(movie_id :int= Path(gt=0)):
     """this api is to delete a movie with a specific movie_id"""
+    movie_deleted = False
     for i in range(len(MOVIES)):
         if MOVIES[i].id == movie_id:
             MOVIES.pop(i)
+            movie_deleted = True
             break
+    if not movie_deleted:
+        raise HTTPException(status_code=404, detail="item not found")
 
 
-@app.post("/create-movie")
+@app.post("/create-movie", status_code=status.HTTP_201_CREATED)
 def create_movie(movie_request: MovieRequest):
     """this api endpoint is to create a movie"""
     new_movie = Movie(**movie_request.model_dump())
