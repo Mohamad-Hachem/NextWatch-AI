43c9a67ca12c44ec0d0d299478a47872274ca4fe
Mohamad-Hachem <mohamad27hachem@outlook.com>
2026-06-19 12:15:27 +0200

    adding update endpoints for our movies

--- src/app3.py
+++ src/app3.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, HTTPException, Path
+from fastapi import FastAPI, Depends, HTTPException, Path, Query
 import models
 from databse import engine
 from databse import SessionLocal
@@ -43,6 +43,24 @@ def get_movie_by_id(db: db_dependency, movie_id: int = Path(gt=0)):
 def create_movie(db: db_dependency, movie_request: MovieRequest):
     """creating a new movie endpoint"""
     movie = Movies(**movie_request.model_dump())
+
+    db.add(movie)
+    db.commit()
+
+
+@app.put("/movies/update_movie/", status_code=status.HTTP_204_NO_CONTENT)
+def update_movie(db: db_dependency, movie_request: MovieRequest, movie_id: int = Query(gt=0)):
+    """updating movie in database endpoint"""
+    movie = db.query(Movies).filter(Movies.id == movie_id).first()
+
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found to be updated")
+    
+    movie.name = movie_request.name
+    movie.author = movie_request.author
+    movie.description = movie_request.description
+    movie.rating = movie_request.rating
+    movie.category = movie_request.category
 
     db.add(movie)
     db.commit()
