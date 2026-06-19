8030064364f593a11080f30af560ef54cd274a90
Mohamad-Hachem <mohamad27hachem@outlook.com>
2026-06-19 11:33:50 +0200

    adding more apis for data fetching

--- src/app3.py
+++ src/app3.py
@@ -1,10 +1,11 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
 from databse import engine
 from databse import SessionLocal
 from models import Movies
+from starlette import status
 
 
 app = FastAPI()
@@ -22,7 +23,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def get_all_movies(db: db_dependency):
     """returning all the movies in our Database"""
     return db.query(Movies).all()
+
+
+@app.get("/movies/{movie_id}", status_code=status.HTTP_200_OK)
+def get_movie_by_id(db: db_dependency, movie_id: int = Path(gt=0)):
+    """this api endpoint to get a specific movie by id"""
+    movie = db.query(Movies).filter(Movies.id == movie_id).first()
+    if movie:
+        return movie
+    raise HTTPException(status_code=404, detail='Movie not found')
